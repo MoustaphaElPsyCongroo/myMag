ace0330187cf2e3070c89c61469d095f9924c42f
MousElPsyCongroo <mtapha.tall@gmail.com>
2024-11-27 23:06:55 +0100

    Remove useless Article model tests, fix formatting

--- models/article.py
+++ models/article.py
@@ -2,56 +2,72 @@
 """
 Holds the representation of an Article
 """
-from models.base_model import BaseModel, Base
+
 from datetime import datetime
-from sqlalchemy import Column, String, Integer, DateTime, ForeignKey, Table
+
+from sqlalchemy import Column, DateTime, ForeignKey, Integer, String, Table
 from sqlalchemy.orm import relationship
 
-liked_article = Table('liked_article', Base.metadata,
-                      Column('user_id', String(60),
-                             ForeignKey('users.id'), primary_key=True),
-                      Column('article_id', String(60),
-                             ForeignKey('articles.id'), primary_key=True))
+from models.base_model import Base, BaseModel
 
-disliked_article = Table('disliked_article', Base.metadata,
-                         Column('user_id', String(60),
-                                ForeignKey('users.id'), primary_key=True),
-                         Column('article_id', String(60),
-                                ForeignKey('articles.id'), primary_key=True))
+liked_article = Table(
+    "liked_article",
+    Base.metadata,
+    Column("user_id", String(60), ForeignKey("users.id"), primary_key=True),
+    Column(
+        "article_id", String(60), ForeignKey("articles.id"), primary_key=True
+    ),
+)
 
-read_article = Table('read_article', Base.metadata,
-                     Column('user_id', String(60),
-                            ForeignKey('users.id'), primary_key=True),
-                     Column('article_id', String(60),
-                            ForeignKey('articles.id'), primary_key=True))
+disliked_article = Table(
+    "disliked_article",
+    Base.metadata,
+    Column("user_id", String(60), ForeignKey("users.id"), primary_key=True),
+    Column(
+        "article_id", String(60), ForeignKey("articles.id"), primary_key=True
+    ),
+)
+
+read_article = Table(
+    "read_article",
+    Base.metadata,
+    Column("user_id", String(60), ForeignKey("users.id"), primary_key=True),
+    Column(
+        "article_id", String(60), ForeignKey("articles.id"), primary_key=True
+    ),
+)
 
 
 class Article(BaseModel, Base):
     """Representation of an Article"""
-    __tablename__ = 'articles'
-    feed_id = Column(String(60), ForeignKey('feeds.id'), nullable=False)
+
+    __tablename__ = "articles"
+    feed_id = Column(String(60), ForeignKey("feeds.id"), nullable=False)
     title = Column(String(256), nullable=False)
     image = Column(String(2083), nullable=True)
     link = Column(String(2083), nullable=False)
-    description = Column(String(2000), default='', nullable=False)
+    description = Column(String(2000), default="", nullable=False)
     publish_date = Column(DateTime, nullable=False)
     shares = Column(Integer, default=0, nullable=False)
-    article_feed = relationship('Feed', back_populates='feed_articles')
+    article_feed = relationship("Feed", back_populates="feed_articles")
     article_liked_by = relationship(
-        'User', secondary=liked_article, back_populates='liked_articles')
+        "User", secondary=liked_article, back_populates="liked_articles"
+    )
     article_disliked_by = relationship(
-        'User', secondary=disliked_article, back_populates='disliked_articles')
+        "User", secondary=disliked_article, back_populates="disliked_articles"
+    )
     article_read_by = relationship(
-        'User', secondary=read_article, back_populates='read_articles'
+        "User", secondary=read_article, back_populates="read_articles"
     )
     article_tag_associations = relationship(
-        'TagArticleAssociation',
-        back_populates='article',
-        cascade='all, delete')
+        "TagArticleAssociation",
+        back_populates="article",
+        cascade="all, delete",
+    )
     article_user_score_associations = relationship(
-        'ArticleUserScoreAssociation',
-        back_populates='article',
-        cascade='all, delete'
+        "ArticleUserScoreAssociation",
+        back_populates="article",
+        cascade="all, delete",
     )
 
     def __init__(self, *args, **kwargs):
@@ -62,13 +78,16 @@ class Article(BaseModel, Base):
 class ArticleUserScoreAssociation(Base):
     """Association of an Article to a User, with extra data
     (here score and subsection membership)"""
-    __tablename__ = 'article_score'
-    user_id = Column(ForeignKey('users.id'), primary_key=True)
-    article_id = Column(ForeignKey('articles.id'), primary_key=True)
+
+    __tablename__ = "article_score"
+    user_id = Column(ForeignKey("users.id"), primary_key=True)
+    article_id = Column(ForeignKey("articles.id"), primary_key=True)
     score_from_tags = Column(Integer, default=0, nullable=False)
     total_score = Column(Integer, default=100, nullable=False)
     last_scoring_date = Column(DateTime, default=datetime.now, nullable=False)
     user = relationship(
-        'User', back_populates='user_article_score_associations')
+        "User", back_populates="user_article_score_associations"
+    )
     article = relationship(
-        'Article', back_populates='article_user_score_associations')
+        "Article", back_populates="article_user_score_associations"
+    )
--- tests/test_models/test_article.py
+++ tests/test_models/test_article.py
@@ -3,73 +3,82 @@
 Tests for the Article model
 """
 
-from decouple import config
 import inspect
-from models.base_model import BaseModel
-from models.article import Article
-from models.user import User
-from models.feed import Feed
-from models.tag import Tag, TagArticleAssociation
-from models import storage
-import pycodestyle
 import unittest
-from datetime import datetime
+
+import pycodestyle
+from decouple import config
+
+from models.article import Article
+from models.base_model import BaseModel
 
 
 class TestArticleDocs(unittest.TestCase):
     """Tests to check the documentation and style of Article class"""
+
     @classmethod
     def setUpClass(cls):
         """Set up for the doc tests"""
         cls.article_f = inspect.getmembers(Article, inspect.isfunction)
 
     def setUp(self):
-        if config('MYSQL_ENV') != 'test':
+        if config("MYSQL_ENV") != "test":
             self.fail(
                 """You're on the prod database.
-                Edit .env to test on the right database""")
+                Edit .env to test on the right database"""
+            )
 
     def test_pycodestyle_conformance_article(self):
         """Test that models/article.py conforms to pycodestyle."""
         pycodestyles = pycodestyle.StyleGuide(quiet=True)
-        result = pycodestyles.check_files(['models/article.py'])
-        self.assertEqual(result.total_errors, 0,
-                         "Found code style errors (and warnings).")
+        result = pycodestyles.check_files(["models/article.py"])
+        self.assertEqual(
+            result.total_errors, 0, "Found code style errors (and warnings)."
+        )
 
     def test_pycodestyle_conformance_test_article(self):
         """Test that
         tests/test_models/test_article.py conforms to pycodestyle."""
         pycodestyles = pycodestyle.StyleGuide(quiet=True)
         result = pycodestyles.check_files(
-            ['tests/test_models/test_article.py'])
-        self.assertEqual(result.total_errors, 0,
-                         "Found code style errors (and warnings).")
+            ["tests/test_models/test_article.py"]
+        )
+        self.assertEqual(
+            result.total_errors, 0, "Found code style errors (and warnings)."
+        )
 
     def test_article_module_docstring(self):
         """Test for the article.py module docstring"""
-        self.assertIsNot(Article.__doc__, None,
-                         "article.py needs a docstring")
-        self.assertTrue(len(Article.__doc__) >= 1,
-                        "article.py needs a docstring")
+        self.assertIsNot(Article.__doc__, None, "article.py needs a docstring")
+        self.assertTrue(
+            len(Article.__doc__) >= 1, "article.py needs a docstring"
+        )
 
     def test_article_class_docstring(self):
         """Test for the article class docstring"""
-        self.assertIsNot(Article.__doc__, None,
-                         "article class needs a docstring")
-        self.assertTrue(len(Article.__doc__) >= 1,
-                        "article class needs a docstring")
+        self.assertIsNot(
+            Article.__doc__, None, "article class needs a docstring"
+        )
+        self.assertTrue(
+            len(Article.__doc__) >= 1, "article class needs a docstring"
+        )
 
     def test_article_func_docstrings(self):
         """Test for the presence of docstrings in article methods"""
         for func in self.article_f:
-            self.assertIsNot(func[1].__doc__, None,
-                             "{:s} method needs a docstring".format(func[0]))
-            self.assertTrue(len(func[1].__doc__) >= 1,
-                            "{:s} method needs a docstring".format(func[0]))
+            self.assertIsNot(
+                func[1].__doc__,
+                None,
+                "{:s} method needs a docstring".format(func[0]),
+            )
+            self.assertTrue(
+                len(func[1].__doc__) >= 1,
+                "{:s} method needs a docstring".format(func[0]),
+            )
 
 
 class TestArticle(unittest.TestCase):
-    """Tests for the Article class"""
+    """Tests for the Article model"""
 
     def test_is_subclass(self):
         """Test that Article is a subclass of BaseModel"""
@@ -84,106 +93,20 @@ class TestArticle(unittest.TestCase):
         article = Article()
         self.assertTrue(hasattr(article, "feed_id"))
         self.assertTrue(hasattr(article, "title"))
+        self.assertTrue(hasattr(article, "image"))
+        self.assertTrue(hasattr(article, "link"))
         self.assertTrue(hasattr(article, "description"))
         self.assertTrue(hasattr(article, "publish_date"))
         self.assertTrue(hasattr(article, "shares"))
+        self.assertTrue(hasattr(article, "article_feed"))
         self.assertTrue(hasattr(article, "article_liked_by"))
         self.assertTrue(hasattr(article, "article_disliked_by"))
         self.assertTrue(hasattr(article, "article_read_by"))
         self.assertTrue(hasattr(article, "article_tag_associations"))
-
-    def test_to_dict_creates_dict(self):
-        """Test that to_dict method creates a dictionary"""
-        ar = Article()
-        new_d = ar.to_dict()
-        self.assertEqual(type(new_d), dict)
-        for attr in ar.__dict__:
-            if attr != '_sa_instance_state':
-                self.assertTrue(attr in new_d)
-        self.assertTrue("__class__" in new_d)
+        self.assertTrue(hasattr(article, "article_user_score_associations"))
 
     def test_str(self):
         """test that the str method has the correct output"""
         article = Article()
         string = "[Article] ({}) {}".format(article.id, article.__dict__)
         self.assertEqual(string, str(article))
-
-    def test_like(self):
-        """Test article likes"""
-        feed = Feed(
-            name="FeedName",
-            link="FeedUrl"
-        )
-
-        storage.new(feed)
-
-        article = Article(
-            feed_id=feed.id,
-            title="ArticleTitle",
-            description="ArticleDescription",
-            publish_date=datetime.now(),
-            shares=150
-        )
-
-        user = User(
-            email="UserEmail",
-            name='UserName',
-            profile_pic="UserProfilepic"
-        )
-
-        storage.new(article)
-        storage.new(user)
-        article.article_liked_by.append(user)
-
-        storage.save()
-
-        found_user = storage.get(User, user.id)
-        self.assertTrue(article in found_user.liked_articles)
-
-        dummy = Article(
-            feed_id=feed.id,
-            title="ArticleTitle",
-            description="ArticleDescription",
-            publish_date=datetime.now(),
-            shares=50
-        )
-
-        storage.new(dummy)
-
-        self.assertFalse(dummy in found_user.liked_articles)
-
-    def test_tag_associations(self):
-        """Test article tag Association object"""
-        feed = Feed(
-            name="FeedName",
-            link="FeedUrl"
-        )
-
-        storage.new(feed)
-
-        article = Article(
-            feed_id=feed.id,
-            title="ArticleTitle",
-            description="ArticleDescription",
-            publish_date=datetime.now(),
-            shares=150
-        )
-
-        storage.new(article)
-
-        assoc = TagArticleAssociation(confidence=0.95)
-        assoc.tag = Tag(
-            name="TagName",
-            type='Tag'
-        )
-
-        article.article_tag_associations.append(assoc)
-        storage.save()
-
-        for asso in article.article_tag_associations:
-            if assoc.tag.id == asso.tag.id:
-                confidence = asso.confidence
-                tag = asso.tag
-
-        self.assertEqual(confidence, 0.95)
-        self.assertEqual(assoc.tag, tag)
